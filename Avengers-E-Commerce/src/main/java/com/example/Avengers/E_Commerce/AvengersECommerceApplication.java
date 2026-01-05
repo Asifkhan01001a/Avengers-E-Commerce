41ed3ca0572ef0d80a62290a21076f7ed2bd7d63
asif <asifkhan68138@gmail.com>
2026-01-05 10:37:13 +0530

    project Started

---
+++ Avengers-E-Commerce/src/main/java/com/example/Avengers/E_Commerce/AvengersECommerceApplication.java
@@ -0,0 +1,15 @@
+package com.example.Avengers.E_Commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersECommerceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AvengersECommerceApplication.class, args);
+
+	}
+
+}
---
+++ Avengers-E-Commerce/src/test/java/com/example/Avengers/E_Commerce/AvengersECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Avengers.E_Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
